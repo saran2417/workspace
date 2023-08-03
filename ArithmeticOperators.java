f639db929fb22c136a2e0673d0748077074cf1a4
saran2417 <saran.git005@gmail.com>
2023-08-03 10:39:39 +0530

    Arithmetic Operators Program

---
+++ ArithmeticOperators.java
@@ -0,0 +1,16 @@
+package com.corejava;
+
+public class ArithmeticOperators {
+
+	public static void main(String[] args) {
+		int a=15;
+		int b= 3;
+		System.out.println("Addition : "+(a+b));
+		System.out.println("Subtraction : "+(a-b));
+		System.out.println("Multiplication : "+(a*b));
+		System.out.println("Division : "+(a/b));
+		System.out.println("Modlus : "+(a%b));
+
+	}
+
+}
